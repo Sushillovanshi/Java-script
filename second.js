914bb58bbe03f0b2b5519267305f18977064b9a7
Sushil <sushillovanshi8@gamil.com>
2024-03-09 18:36:00 +0530

    .

--- second.js
+++ second.js
@@ -7,7 +7,7 @@ for(let i=1; i<=a; i++)
 
 // ======================================================================================//
 
-let a=parseInt(prompt("Enter a number"))
+let parseInt(prompt("Enter a number"))
 if(a>=1 && a<=10)
 {
     for(let i=1 ; i<=a; i++)
