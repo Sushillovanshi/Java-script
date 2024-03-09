5241d80f129004616c5cf6a66409df21fb994556
Sushil <sushillovanshi8@gamil.com>
2024-03-09 18:21:35 +0530

    third.html form

---
+++ third.js
@@ -0,0 +1,11 @@
+let name = document.getElementById('name').value;
+let contact = document.getElementById('contact').value;
+let password = document.getElementById('password').value;
+let cpass = document.getElementById('cpass').value;
+
+function validate(){
+    if(name==""){
+        alert("please fill ur namer")
+        return false;
+    }
+}
