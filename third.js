a2823b85d4466c60f46bed5cf8fbc61c7029d1b1
Sushil <sushillovanshi8@gamil.com>
2024-03-12 18:39:57 +0530

    12/3/24

--- third.js
+++ third.js
@@ -1,28 +1,44 @@
-let name = document.getElementById('name').value;
-let contact = document.getElementById('contact').value;
-let password = document.getElementById('password').value;
-let cpass = document.getElementById('cpass').value;
+// //let name = document.getElementById('name').value;
+// let contact = document.getElementById('contact').value;
+// let password = document.getElementById('password').value;
+// let cpass = document.getElementById('cpass').value;
 
-function validate(){
-    if(name==""){
-        alert("please fill ur namer")
-        return false;
-    }
+// function validate(){
+//     if(name==""){
+//         alert("please fill ur namer")
+//         return false;
+//     }
+// }
+
+// function valid(){
+
+//     if (name==) 
+//     {
+//         alert("please enter a name")
+//         return false;
+        
+//     }
+//     else(contact==)
+//     {
+//         alert("please enter ur conact")
+//         return false;
+
+//     }
+    
+// }
+
+// =============================================================================//
+let c = document.getElementById('count');
+let v = 0;
+function inc(){
+    v++;
+    c.innerHTML=v;
 }
 
-function valid(){
-
-    if (name==) 
-    {
-        alert("please enter a name")
-        return false;
-        
-    }
-    else(contact==)
-    {
-        alert("please enter ur conact")
-        return false;
-
+function dec(){
+    if(v>0){
+    v--;
+    c.innerHTML=v;
     }
     
-}
+    }
