848a0bde5da216de93a370fb484ed0401175ef12
Sushil <sushillovanshi8@gamil.com>
2024-03-15 18:36:28 +0530

    re

---
+++ sateinterval.js
@@ -0,0 +1,26 @@
+// let b= setInterval(()=>{document.write("Hello Rogue" + " <br> ")},1000)
+// let a= setInterval(()=>console.log("hello"),1000);   // (function(function)) higer order, call back function.
+//    setTimeout(()=>clearInterval(a),4000);
+// let b = new Date();
+// console.log(b.toLocaleDateString());   // local machione date 
+// console.log(b.getDate());
+// console.log(b.getFullYear());
+// console.log(b.getHours());
+// console.log(b.getMonth());
+// console.log(b.getTimezoneOffset());
+
+// // setinterval is time second
+
+// setInterval(() => {
+//     let c = new Date();
+//     let d= c.toLocaleTimeString();
+//     console.log(d)
+    
+// },1000 );
+
+let a = setInterval(()=>{document.write(0)},100)
+setInterval(() => {
+    let a = new Number();
+    console.log(a)
+}, interval);
+// let b = document.getElementById('stop');
