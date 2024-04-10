4aaff95e94254bc9f9b207cda96d53a77b528cb2
Sushil <sushillovanshi8@gamil.com>
2024-04-10 18:06:17 +0530

    update

--- object/arrayobject.js
+++ object/arrayobject.js
@@ -1,44 +1,71 @@
-let student = [
+// let student = [
+//     {
+//         sname: "sushil",
+//         sroll: 10,
+//         sage: 21
+//     },
+//     {
+//         sname: "sushant",
+//         sroll: 11,
+//         sage: 22
+//     },
+//     {
+//         sname: "sunil",
+//         sroll: 12,
+//         sage: 22
+//     }
+// ]
+// console.log("after using array object");
+// console.log(student);
+
+
+// student.push({
+
+//     sname: "sush",
+//     sroll: 101,
+//     sage: 25
+// });
+// console.log("after using push ");
+// console.log(student);
+// student.indexOf({ sname: "sushil", sroll: 16, sage: 36 })
+// console.log("after using indexof");
+// console.log(student); endl;
+
+// student.pop({})
+// console.log("after using pop");
+// console.log(student); endl;
+
+// student.forEach((i)=> console.log(i));   
+// // for Each is a loop as well as function  allows you to 
+// // iterate over an array and perform an operation on each element of the array.
+
+// student.forEach((a)=> console.log(a.sname));   // printe key on using by foreach..
+// student.forEach((b)=> console.log(Object.values(b))); // printe values on using by foreach..
+
+
+
+//     ============ // 10/4/2024   =========================
+
+
+let emp = [
     {
-        sname: "sushil",
-        sroll: 10,
-        sage: 21
+        name: "sushil",
+        age: 21,
+        status: "singal"
     },
     {
-        sname: "sushant",
-        sroll: 11,
-        sage: 22
+        name: "nitin",
+        age: 24,
+        stauts: "mingal"
     },
     {
-        sname: "sunil",
-        sroll: 12,
-        sage: 22
+        name: "nitin",
+        age: 25,
+        stauts: "mingal"
     }
 ]
-console.log("after using array object");
-console.log(student);
+//  let p = emp.map((i)=>{return i.name})
+//  console.log(p);
 
-
-student.push({
-
-    sname: "sush",
-    sroll: 101,
-    sage: 25
-});
-console.log("after using push ");
-console.log(student);
-student.indexOf({ sname: "sushil", sroll: 16, sage: 36 })
-console.log("after using indexof");
-console.log(student); endl;
-
-student.pop({})
-console.log("after using pop");
-console.log(student); endl;
-
-student.forEach((i)=> console.log(i));   
-// for Each is a loop as well as function  allows you to 
-// iterate over an array and perform an operation on each element of the array.
-
-student.forEach((a)=> console.log(a.sname));   // printe key on using by foreach..
-student.forEach((b)=> console.log(Object.values(b))); // printe values on using by foreach..
-
+let m = emp.filter((t) => { return t.age > 21 && t.age< 25})
+console.log(m);
