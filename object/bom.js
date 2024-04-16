07a92d707bdcddc1359d132e319057fec95b489e
Sushil <sushillovanshi8@gamil.com>
2024-04-16 18:37:53 +0530

    m

---
+++ object/bom.js
@@ -0,0 +1,14 @@
+// function su() {
+//     window.print();
+// }
+
+// //  =============  open new window & close new windows =====================
+
+// var m
+// function op() {
+//     window.open("http://www.google.com", "_blank", "width=500px");
+
+// }
+// function cls() {
+//     matchMedia.cls()
+// }
