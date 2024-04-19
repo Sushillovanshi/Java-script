18d419ffe8079bde277da121501facb6ab288604
Sushil <sushillovanshi8@gamil.com>
2024-04-19 18:33:15 +0530

    add new file  on json

--- JSON/Apifetch.js
+++ JSON/Apifetch.js
@@ -1,11 +1,11 @@
-async function fet(){
-    let  a = await fetch ("https://jsonplaceholder.typicode.com/posts")
+async function first(){
+    let  a = await fetch ("http://localhost:3000/student")
     let result = await a.json()
     console.log(result,"\n");
     console.log(result.map((e)=>e.id));
 
 }
-fet()
+first()
 
 // JOSN =>>  javaScript object notation  :
 
