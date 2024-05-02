615a2e716d1dcd11d2d83c4f1c914565fbe42ff6
Sushil <sushillovanshi8@gamil.com>
2024-05-02 17:34:50 +0530

    add on new file on js

---
+++ dom.js
@@ -0,0 +1,18 @@
+let r = document.getElementById("head1")
+r.innerHTML="hi rouge"
+r.style.backgroundColor="blue"
+r.style.color="white"
+
+let e = document.getElementById("head2")
+e.innerHTML="Hi Sushil"
+e.style.backgroundColor=" red"
+e.style.color="white"
+e.style.fontSize="20px"
+
+let d = document.getElementById("head3")
+d.innerHTML="dsvbsvhskjvhdcnkcjshfshclcsfijklhsf"
+d.style.backgroundColor="lightblue"
+d.style.color="green"
+
+let im = document.getElementById("imm")
+im.src="img/IMG_5569.JPG"
